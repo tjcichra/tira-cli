b389891885554aa9cfb6590407b69e3aca9cbe76
tjcichra <8120075+tjcichra@users.noreply.github.com>
2022-02-12 11:26:24 -0500

    added basic box

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,48 @@
-use std::collections::HashMap;
-
-use crate::models::User;
+use std::{io, time::Duration, thread};
+use tui::{
+    backend::CrosstermBackend,
+    widgets::{Widget, Block, Borders},
+    layout::{Layout, Constraint, Direction},
+    Terminal
+};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
 
 mod models;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("http://localhost:8000/users")
-        .await?
-        .json::<Vec<User>>()
-        .await?;
-    println!("{:#?}", resp);
+fn main() -> Result<(), io::Error> {
+    // let resp = reqwest::get("http://localhost:8000/users")
+    //     .await?
+    //     .json::<Vec<User>>()
+    //     .await?;
+    // println!("{:#?}", resp);
+    enable_raw_mode()?;
+
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default()
+            .title("Example")
+            .borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
+
+    thread::sleep(Duration::from_millis(5000));
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
     Ok(())
 }
